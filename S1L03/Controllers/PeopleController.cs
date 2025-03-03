7ac5740bb1409487f82271ca2a5d1692c332edda
kezia <kezia@KEZIA_SEGURADO>
2025-03-03 18:03:47 +0200

    Add project files.

---
+++ S1L03/Controllers/PeopleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace S1L03.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+
+        public ActionResult ListPeople()
+        {
+            List<Models.PersonModel> people = new List<Models.PersonModel>();
+
+            people.Add(new Models.PersonModel { studentNumber = "u01234567", FirstName = "Jane", LastName = "Doe", email = "jane.doe@tuks.co.za" });
+            people.Add(new Models.PersonModel { studentNumber = "u12345678", FirstName = "John", LastName = "Doe", email = "john.doe@tuks.co.za " });
+            people.Add(new Models.PersonModel { studentNumber = "u24697531", FirstName = "Sara", LastName = "Smith", email = "Sara.Smith.@tuks.co.za" });
+            people.Add(new Models.PersonModel { studentNumber = "u45976213", FirstName = "Bianca", LastName = "Du Tiot", email = "Bianca.Dutoit@tuks.co.za" });
+            people.Add(new Models.PersonModel { studentNumber = "u79435884", FirstName = "Rachel", LastName = "Van Der Merve", email = "Rachel.Vandermerve@tuks.co.za" });
+
+            return View(people);
+
+        }
+ 
+    }
+}
---
+++ S1L03/Models/PersonModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations; // Add to project
+//Ensure that CodeLens is activated
+//Select >> Tools >> Options >> Text Editor >> All Languages >> CodeLens
+namespace S1L03.Models
+{
+    public class PersonModel
+    {
+        //Quick property - type "prop" press tab x2
+        //Complete the property details
+        //This is the "Model" of "data source"
+
+        [Display(Name = "Student Number")] //Add as decorator
+        public string studentNumber { get; set; }
+
+        [Display(Name = "First Name")] //Add as decorator
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")] //Add as decorator
+        public string LastName { get; set; }
+
+        [Display(Name = "Email Address")] //Add as decorator
+        public string email { get; set; }
+    }
+}
